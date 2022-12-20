0520771725fd51d63180828536ee1cbb6c16db1a
kishor kumar <kk96313044@gmail.com>
2022-12-20 10:26:56 -0800

    variable js

---
+++ veriable.js
@@ -0,0 +1,54 @@
+// let a= 'abc'; //string
+// console.log(typeof a);
+
+
+// let b= 1234;//number
+// console.log(typeof b);
+ 
+
+// let a= true; //boolean
+// console.log(typeof a);
+
+
+// let a= [1,2,3,4,5]; //object 
+// console.log(typeof a);
+
+
+// let a ; // vo value undeffine
+// console.log(typeof a);
+
+
+// let fun = function(){} //function
+// console.log(typeof fun);
+
+
+// let man = {Name :"kishor", age: 23,}; //object datatype
+// console.log(typeof man);
+
+
+// let date = new Date("2022-12-20");
+// console.log(date);
+
+
+//--------------------------------------- difference between var and let
+
+
+/* let a=10;
+ if(true){  // let is block scope lavel
+     let a=13;
+     console.log(a);
+ } */
+
+ /* let a=10;
+ if(true){
+    let a=20;
+ }
+console.log(a); */
+
+
+/* var a=10;  // var is global scope
+if(true){
+    var a=20;
+    
+}
+console.log(a); */
