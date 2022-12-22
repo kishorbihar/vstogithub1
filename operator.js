12466c77abc7e831a4b204eff86e2ee5cbf44e8b
kishor kumar <kk96313044@gmail.com>
2022-12-22 02:41:26 -0800

    operator in js

---
+++ operator.js
@@ -0,0 +1,130 @@
+//----------------------Assignment Operators
+// const x = 5;
+// console.log(x)
+
+/* let a = 1;
+a+=5;
+console.log (a); */
+
+/* let b = 5;
+b -= 2;
+console.log(b) */
+
+/* let a=5;
+a *=5;
+console.log(a); */
+
+
+/* let  a=10;
+a /=2;
+console.log(a); */
+
+/* let a=15;
+a %=2;
+console.log(a); */
+
+/* let a=3;
+a **=2;
+console.log(a); */
+
+
+//-------------------------------------airthmatic opperator
+ /* let x=10;
+let y=5;
+let c=x+y;
+console.log(c) */
+
+/* let x=10;
+let y=5;
+console.log(x-y) */
+
+/* let x=10;  // post increment
+let y = x++;
+console.log(y) */
+
+/* let x= 10; //pree increment
+let y = ++x;
+console.log(y) */
+
+/* let x=10;
+let y = --x;
+console.log(y) */
+
+/* let x=10;
+let y = 4;
+console.log (x%y) */
+
+/* let x=10;
+let y=5;
+console.log(x/y) */
+
+/* let x=10;
+let y=5;
+console.log(x**y); */
+
+//----------------------- comparision operator
+/* const a=3;
+const b=2;
+console.log(a<b) */
+
+/*
+let a=3;
+let b=4;
+console.log(a==b); */
+
+/* let a=2;
+let b= '2';
+console.log(a==b)
+console.log(a===b) */
+
+/* let a=3;
+let b=4;
+console.log(a!=b)
+console.log(a>b)
+console.log(b>a)
+console.log(b>=a)
+console.log(a>=b) */
+
+//----------------------------logical operator
+
+/* const x = 5, y = 3;
+console.log((x < 6) && (y < 5)); */
+
+/* const x=3,y=4;
+console.log(x<5 && 5>y);
+console.log(x>1 ||x>y)
+console.log(x>5 && x>1)
+console.log(!true) */
+
+//---------------------------------- string operator
+/* console.log('hello ' + 'kishor');
+
+let a= 'javascript ';
+a += 'is a programing language';
+console.log(a); */
+
+//------------------------------------other operator
+/* let a = (1, 2, 3, 4, 8,7) ;
+console.log(a); */
+
+// console.log((5 > 9) ? 'success' : 'error');
+
+
+
+
+
+//-----------------------betwise operator 
+
+const a = 4;        // 00000000000000000000000000000101
+const b = 3;        // 00000000000000000000000000000011
+
+console.log(a & b);
+
+
+
+
+
+
+
+
+
